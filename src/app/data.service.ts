f5641552dd510444a45716979c2b57665b3b1ec1
obusha <odn99@mail.ru>
2021-02-17 15:37:50 +0700

    Добавила возможность изменения стоимости конкретной тренировки

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,18 +1,18 @@
 const trainerList = [
-  {id: 0, lastName: "Иванов", firstName: "Иван", patronymic: "Иванович", post: "Старший тренер"},
-  {id: 1, lastName: "Петров", firstName: "Петр", patronymic: "Петрович", post: "Младший тренер"}
+  {id: 0, lastName: 'Иванов', firstName: 'Иван', patronymic: 'Иванович', post: 'Старший тренер'},
+  {id: 1, lastName: 'Петров', firstName: 'Петр', patronymic: 'Петрович', post: 'Младший тренер'}
 ];
 
-const trainerPriceList = [
-  {id:0, trainerId: 0, price: 1000},
-  {id:1, trainerId: 1, price: 2000}
-]
+let trainerPriceList = [
+  {id: 0, trainerId: 0, price: 1000},
+  {id: 1, trainerId: 1, price: 2000}
+];
 
-const  trainingSessionList = [
-  {id: 0, name: "Фулбади", trainerId: 1, time: '12:30:00', date: "01.04.2020", trainingTime: 2},
-  {id: 1, name: "Фулбади", trainerId: 0, time: '14:30:00', date: "01.02.2020", trainingTime: 2},
-  {id: 2, name: "Расстяжка", trainerId: 0, time: '14:30:00', date: "01.05.2020", trainingTime: 1},
-  {id: 3, name: "Индивидуальная тренировка", trainerId: 1, time: '16:30:00', date: "01.01.2020", trainingTime: 2}
+const trainingSessionList = [
+  {id: 0, name: 'Фулбади', trainerId: 1, time: '12:30:00', date: '01.04.2020', trainingTime: 2, altPrice: null},
+  {id: 1, name: 'Фулбади', trainerId: 0, time: '14:30:00', date: '01.02.2020', trainingTime: 2, altPrice: null},
+  {id: 2, name: 'Расстяжка', trainerId: 0, time: '14:30:00', date: '01.05.2020', trainingTime: 1, altPrice: null},
+  {id: 3, name: 'Индивидуальная тренировка', trainerId: 1, time: '16:30:00', date: '01.01.2020', trainingTime: 2, altPrice: null}
 ];
 
 export class DataService {
@@ -22,10 +22,25 @@ export class DataService {
   }
 
   getTrainerSessions(trainerId: number): any[] {
-    return trainingSessionList.filter((session)=> session['trainerId'] == trainerId);
+    return trainingSessionList.filter((session) => session['trainerId'] == trainerId);
   }
 
-  getTrainerPrice(trainerId: number): number{
-    return trainerPriceList.filter((price)=> price['trainerId'] == trainerId)[0]['price'];
+  getTrainerPrice(trainerId: number): any[] {
+    return trainerPriceList.filter((price) => price['trainerId'] == trainerId);
+  }
+
+  getTrainerIncome(trainerId: number): number {
+    let sum: number = 0;
+    let price: number = this.getTrainerPrice(trainerId)[0]['price'];
+    let trainerSessions: any[] = this.getTrainerSessions(trainerId);
+    for (let session of trainerSessions) {
+      let session_price = session['altPrice'] ? session['altPrice'] : price;
+      sum += session['trainingTime'] * session_price;
+    }
+    return sum;
+  }
+
+  addAlternativePrice(trainingSessionId: number, altPrice: number): void {
+    trainingSessionList[trainingSessionId]['altPrice'] = altPrice;
   }
 }
---
+++ src/app/trainerDetailPage/trainerDetailPage.component.ts
@@ -0,0 +1,60 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DataService} from '../data.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'details-app',
+  templateUrl: './trainerDetailPage.component.html',
+  styleUrls: ['./trainerDetailPage.component.scss'],
+  providers: [DataService]
+})
+
+export class TrainerDetailPageComponent implements OnInit {
+  @ViewChild('inputPrice') inputAltPrice: ElementRef;
+
+  trainerSessions: any[];
+  price: number;
+  sessionPrice: number;
+  sum: number;
+  id: number;
+  idSession: number;
+  trainerName: string;
+  altPrice: number;
+  focus: boolean = false;
+
+  private subscription: Subscription;
+  private querySubscription: Subscription;
+
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
+    this.subscription = route.params.subscribe(params => this.id = params['id']);
+    this.querySubscription = route.queryParams.subscribe((queryParam: any) => {
+      this.trainerName = queryParam['trainerName'];
+    });
+  }
+
+  ngOnInit() {
+    this.sum = 0;
+    this.trainerSessions = this.dataService.getTrainerSessions(this.id);
+    this.price = this.dataService.getTrainerPrice(this.id)[0]['price'];
+    this.sum = this.dataService.getTrainerIncome(this.id);
+  }
+
+  addFocus(sessionId: number) {
+    this.idSession = sessionId;
+    console.log(this.idSession);
+    this.focus = true;
+    setTimeout(() => { // this will make the execution after the above boolean has changed
+      this.inputAltPrice.nativeElement.focus();
+    }, 0);
+  }
+
+
+  addAltPrice(sessionId: number, altPrice: number, trainerId: number) {
+    if (altPrice > 0) {
+      this.dataService.addAlternativePrice(sessionId, altPrice);
+      this.sum = this.dataService.getTrainerIncome(trainerId);
+    }
+    this.focus = false;
+  }
+}
